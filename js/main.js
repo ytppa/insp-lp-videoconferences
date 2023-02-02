437e1e24f724a9de16a72e41929997078c7069e0
a.gutov <ytppa@yandex.ru>
2023-02-02 15:00:33 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+AOS.init();
 
 
+(function(){
+
+const scroll = function(area) { 
+    
+    var sizes = {
+        height: area.height(),
+        scrollheight: area.prop("scrollHeight"),
+        scrolltop: area.scrollTop(),
+        bottom: 0
+    };
+    sizes.bottom = sizes.scrollheight - sizes.height - sizes.scrolltop;
+
+    return sizes;
+}
+
+window.on("scroll", e => console.log(scroll()))
+
+})()
