29fd16dc6b49dc2f3aa5572399ad91386e332075
a.gutov <ytppa@yandex.ru>
2023-02-06 22:01:33 +0300

    Секция успешно "зависает" при скролле. - Доделываю анимацию согласно newPos параметру

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 
-(function(){
+(function () {
 
     /**
      * Menu behavior
@@ -14,7 +14,7 @@ AOS.init();
             this._sticked = false;
 
             const that = this;
-            document.addEventListener("scroll", () => {that.onScroll()});
+            document.addEventListener("scroll", () => { that.onScroll() });
         }
 
         get sticked() {
@@ -51,7 +51,7 @@ AOS.init();
     }
 
     let menuToggler = new Toggler(
-        document.querySelector("#topMenu"), 
+        document.querySelector("#topMenu"),
         document.querySelector("#cta-1"),
         "is-sticked"
     );
@@ -59,110 +59,110 @@ AOS.init();
     /**
      * Vertical scrolling gallery
      */
-    const scrollingContainer = document.querySelector(".note-w-text");
+    // const scrollingContainer = document.querySelector(".note-w-text");
 
-    const process_touchstart = e => console.log("~ touchstart");
-    const process_touchcancel = e => console.log("~ touchcancel");
-    const process_touchend = e => console.log("~ touchend");
-    const process_touchmove = e => {
-        switch (e.touches.length) {
-            case 1: console.log(`handle_one_touch`, e); break;
-            case 2: console.log(`handle_two_touches`, e); break;
-            case 3: console.log(`handle_three_touches`, e); break;
-            default: console.log(`gesture_not_supported`, e); break;
-        }
-        console.log("~ touchmove");
-    }
+    // const process_touchstart = e => console.log("~ touchstart");
+    // const process_touchcancel = e => console.log("~ touchcancel");
+    // const process_touchend = e => console.log("~ touchend");
+    // const process_touchmove = e => {
+    //     switch (e.touches.length) {
+    //         case 1: console.log(`handle_one_touch`, e); break;
+    //         case 2: console.log(`handle_two_touches`, e); break;
+    //         case 3: console.log(`handle_three_touches`, e); break;
+    //         default: console.log(`gesture_not_supported`, e); break;
+    //     }
+    //     console.log("~ touchmove");
+    // }
 
-    scrollingContainer.addEventListener("touchstart", process_touchstart, false);
-    scrollingContainer.addEventListener("touchmove", process_touchmove, false);
-    scrollingContainer.addEventListener("touchcancel", process_touchcancel, false);
-    scrollingContainer.addEventListener("touchend", process_touchend, false);
+    // scrollingContainer.addEventListener("touchstart", process_touchstart, false);
+    // scrollingContainer.addEventListener("touchmove", process_touchmove, false);
+    // scrollingContainer.addEventListener("touchcancel", process_touchcancel, false);
+    // scrollingContainer.addEventListener("touchend", process_touchend, false);
 
     //#region ScrollingGallery
     class ScrollingGallery {
         constructor(main, cont, gallery, num) {
-           this.main = main;
-           this.cont = cont;
-           this.gallery = gallery;
-     
-           /**
-            * In this example will show 4 different key state for the gallery.
-            * We'll decide that the container ("timeline") should be
-            * in 4 times large than the gallery.
-            */
-           this.num = num;
-     
-           this.init();
-     
-           this.scrollListener = this.main.addEventListener(
-              "scroll",
-              this.onScroll.bind(this)
-           );
-     
-           this.resizeListener = window.addEventListener(
-              "resize",
-              this.resize.bind(this)
-           );
+            this.main = main;
+            this.cont = cont;
+            this.gallery = gallery;
+
+            // Amount of slides
+            this.num = num;
+
+            // Seting the gallery container size  
+            this.setupContainer();
+
+            this.cont.style.position = "relative";
+            this.gallery.style.position = "absolute";
+            this.gallery.style.top = "0";
+            this.gallery.style.left = "0";
+            this.gallery.style.right = "0";
+
+            // Listeners
+            this.scrollListener = this.main.addEventListener(
+                "scroll",
+                this.onScroll.bind(this)
+            );
+
+            this.resizeListener = window.addEventListener(
+                "resize",
+                this.resize.bind(this)
+            );
         }
-     
+
         onScroll(e) {
-           const scrollTop = this.main.scrollTop,
-              contSize = this.cont.getBoundingClientRect(),
-              gallerySize = this.gallery.getBoundingClientRect(),
-              mainSize = this.main.getBoundingClientRect();
-     
-           let newPos = (window.innerHeight - gallerySize.height) / 2 - contSize.top;
-     
-           if (newPos > 0 && newPos < contSize.height - gallerySize.height) {
-              // Center of screen
-              this.move(newPos);
-           } else if (newPos <= 0) {
-              // Stick to the top of container
-              this.move(0);
-           } else {
-              // Stick to the bottom of container
-              this.move(contSize.height - gallerySize.height);
-           }
+            console.log("~ Scroll");
+            const scrollTop = this.main.scrollTop,
+                contSize = this.cont.getBoundingClientRect(),
+                gallerySize = this.gallery.getBoundingClientRect(),
+                mainSize = this.main.getBoundingClientRect();
+
+            let newPos = (window.innerHeight - gallerySize.height) / 2 - contSize.top;
+            console.log(newPos);
+
+            if (newPos > 0 && newPos < contSize.height - gallerySize.height) {
+                // Center of screen
+                this.move(newPos);
+            } else if (newPos <= 0) {
+                // Stick to the top of container
+                this.move(0);
+            } else {
+                // Stick to the bottom of container
+                this.move(contSize.height - gallerySize.height);
+            }
         }
-     
+
         move(y) {
-           console.log(`move ${y}`);
-     
-           this.gallery.style.transform = `translate(-50%, ${y}px)`;
-     
-           /**
-            * Imitating some gallery state changes during scroll
-            * We'll dynamically change the background
-            */
-           const contHeight = this.cont.getBoundingClientRect().height;
-     
-           this.gallery.style.background = `hsl(${
-              (y * 100) / contHeight
-           }, 50%, 50%)`;
-     
-           this.gallery.innerHTML = `${Math.floor(y)}px`;
+            console.log(`move ${y}`);
+
+            this.gallery.style.transform = `translateY(${y}px)`;
+
+            /**
+             * Imitating some gallery state changes during scroll
+             * We'll dynamically change the background
+             */
         }
-     
+
         resize() {
-           console.log(`~ resize`);
-           this.init();
+            console.log(`~ resize`);
+            this.setupContainer();
         }
-     
-        init() {
-           console.log(`~ init`);
-           const galleryHeight = this.gallery.getBoundingClientRect().height;
-     
-           this.cont.style.height = `${this.num * galleryHeight}px`;
-           this.onScroll();
+
+        setupContainer() {
+            console.log(`~ init`);
+            const galleryHeight = this.gallery.getBoundingClientRect().height;
+
+            this.cont.style.height = `${this.num * galleryHeight}px`;
+            this.onScroll();
         }
     }
     //#endregion ScrollingGallery
-    // let scrollingGallery = new ScrollingGallery(
-    //     document.querySelector(".main"),
-    //     document.querySelector(".gallery"),
-    //     document.querySelector(".gallery--main"),
-    //     4
-    // )
+
+    let scrollingGallery = new ScrollingGallery(
+        document.querySelector(".root"),
+        document.querySelector(".possibilities"),
+        document.querySelector(".possibilities--cont"),
+        5
+    )
 
 })()
